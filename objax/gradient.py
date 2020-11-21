7a42f400fea0b267b1ea160a0f9c4afbf0291a07
Nicholas Carlini <nicholas@carlini.com>
2020-11-21 06:59:22 +0000

    Fix indent

--- objax/gradient.py
+++ objax/gradient.py
@@ -61,7 +61,7 @@ class GradValues(Module):
                 self.vc.assign(original_vc)
 
         assert isinstance(input_argnums, tuple) or input_argnums is None, \
-                f'Must pass a tuple of indices to input_argnums; received {input_argnums}.'
+            f'Must pass a tuple of indices to input_argnums; received {input_argnums}.'
         self.input_argnums = input_argnums or tuple()
 
         signature = inspect.signature(f)
